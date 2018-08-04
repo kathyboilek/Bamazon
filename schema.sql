37738d68cb315ceac259431ac6d57ac20f5bca0e
Kathleen Boilek <kaboilek@gmail.com>
2018-08-04 17:04:48 -0400

    Initial build out of table

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE database bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+  item_id INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
+  price DECIMAL(10,2) NULL,
+  stock_quantity INT NULL,
+  PRIMARY KEY (item_id) 
+);
+
+SELECT * FROM products;
