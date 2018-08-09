0814b809aa45c00b7b8d1bb2cdc3eddad10ce30b
Kathleen Boilek <kaboilek@gmail.com>
2018-08-09 16:28:44 -0400

    fine tuned code and comments

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Bamazon
 An Amazon-like storefront with MySQL skills. CLI application
+
+To run the command line type "node bamazonCustomer.js"
+
+![alt text](https://raw.githubusercontent.com/username/projectname/branch/path/to/img.png)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,83 +10,123 @@ var connection = mysql.createConnection({
     database: "bamazondb"
 });
 
-connection.connect(function(err){
-    if (err) throw err;
-});
 
-var display = function() {
-    connection.query("SELECT * FROM products", function(err, results) {
-        if (err) throw err;
-        console.table(results);
-    })
-};
+function validateInput(value) {
+	var integer = Number.isInteger(parseFloat(value));
+	var sign = Math.sign(value);
 
-var run = function() {
-    // query the database for all products available for purchase
-    connection.query("SELECT * FROM products", function(err, results) {
-        if (err) throw err;
-        // once you have the products, prompt the user for which they'd like to purchase
-        inquirer.prompt([
-            {
-                name: "product",
-                type: "list",
-                choices: function() {
-                    var choiceArray = [];
-                    for (var i = 0; i < results.length; i++) {
-                        choiceArray.push(results[i].product_name);
-                    }
-                    return choiceArray;
-                },
-                message: "What product would you like to purchase?"
-            },
-            {
-                name: "amount",
-                type: "input",
-                message: "How many would you like to purchase?"
-            }
-        ])
-        .then(function(answer) {
-            var chosenProduct;
-            for (var i = 0; i < results.length; i++) {
-                if (results[i].product_name === answer.product) {
-                    chosenProduct = results[i];
-                }
-            }
+	if (integer && (sign === 1)) {
+		return true;
+	} else {
+		return 'Please enter a whole non-zero number.';
+	}
+}
 
-            if (chosenProduct.stock_quantity > parseInt(answer.amount)) {
-                connection.query("UPDATE products SET ? WHERE ?", [
-                {
-                    stock_quantity: chosenProduct.stock_quantity - parseInt(answer.amount)
-                },
-                {
-                    id: chosenProduct.id
-                }], function(error) {
-                    if (error) throw err;
-                    console.log("\n\n");
-                    console.log("==============================================");
-                    console.log("Product purchased successfully!");
-                    console.log("==============================================");
-                    console.log("Purchase Summary");
-                    console.log("-----------------------------");
-                    console.log("Item Name: " +  chosenProduct.product_name);
-                    console.log("Item Count: " + parseInt(answer.amount));
-                    console.log("-----------------------------");
-                    console.log("Total: " + "$" + (chosenProduct.price * parseInt(answer.amount)));
-                    console.log("==============================================");
-                    console.log("\n\n");
-                    display();
-                    run();
-                })
-            } else {
-                console.log("==============================================");
-                console.log("Insufficient stock.");
-                console.log("==============================================");
-                display();
-                run();
-            }
-        });
-    });
-};
+// The app should then prompt users with two messages.
+function promptUserPurchase() {
 
-display();
-run();
+	inquirer.prompt([
+		{
+			type: 'input',
+            name: 'item_id',
+            //The first should ask them the ID of the product they would like to buy.
+			message: 'Please enter the Item ID of the product you would like to buy',
+			validate: validateInput,
+			filter: Number
+		},
+		{
+			type: 'input',
+            name: 'quantity',
+            //The second message should ask how many units of the product they would like to buy.
+			message: 'How many units of the product would you be needing?',
+			validate: validateInput,
+			filter: Number
+		}
+	]).then(function(input) {
+
+		var item = input.item_id;
+		var quantity = input.quantity;
+
+		// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+		var queryStr = 'SELECT * FROM products WHERE ?';
+
+		connection.query(queryStr, {item_id: item}, function(err, data) {
+			if (err) throw err;
+
+			if (data.length === 0) {
+				console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+				displayInventory();
+
+			} else {
+				var productData = data[0];
+
+				if (quantity <= productData.stock_quantity) {
+					console.log('Congratulations, the product you ordered is in stock! Placing order!');
+
+					// Construct the updating query string
+					var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+
+					// Update the inventory
+					connection.query(updateQueryStr, function(err, data) {
+						if (err) throw err;
+
+						console.log('Your order has been placed! Your total is $' + productData.price * quantity);
+						console.log('Thank you for shopping with us!');
+						console.log("\n---------------------------------------------------------------------\n");
+
+						// End the database connection
+						connection.end();
+					})
+				} else {
+                    //If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.S
+					console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
+					console.log('Please modify your order.');
+					console.log("\n---------------------------------------------------------------------\n");
+
+					displayInventory();
+				}
+			}
+		})
+	})
+}
+
+// displayInventory will retrieve the current inventory from the database and output it to the console
+function displayInventory() {
+
+	// Construct the db query string
+	queryStr = 'SELECT * FROM products';
+
+	// Make the db query
+	connection.query(queryStr, function(err, data) {
+		if (err) throw err;
+
+		console.log('Existing Inventory: ');
+		console.log('...................\n');
+
+		var strOut = '';
+		for (var i = 0; i < data.length; i++) {
+			strOut = '';
+			strOut += 'Item ID: ' + data[i].item_id + '  ||  ';
+			strOut += 'Product Name: ' + data[i].product_name + '  ||  ';
+			strOut += 'Department: ' + data[i].department_name + '  ||  ';
+			strOut += 'Price: $' + data[i].price + '\n';
+
+			console.log(strOut);
+		}
+
+	  	console.log("---------------------------------------------------------------------\n");
+
+	  	//Prompt the user for item/quantity they would like to purchase
+	  	promptUserPurchase();
+	})
+}
+
+// runBamazon will execute the main application logic
+function runBamazon() {
+
+	// Display the available inventory
+	displayInventory();
+}
+
+// Run the application logic
+runBamazon();
